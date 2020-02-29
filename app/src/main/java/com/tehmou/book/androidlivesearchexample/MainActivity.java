99e31dbbc58de88a7d644f21e2eeea6279b62328
raka-hadhyana <hadhyana@gmail.com>
2020-02-29 10:57:34 +0700

    fix dependency

--- app/src/main/java/com/tehmou/book/androidlivesearchexample/MainActivity.java
+++ app/src/main/java/com/tehmou/book/androidlivesearchexample/MainActivity.java
@@ -1,10 +1,11 @@
 package com.tehmou.book.androidlivesearchexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.jakewharton.rxbinding2.widget.RxTextView;
 
